4f55afba171cfbfe1040ac7861dc13da8341a3e9
Mykolahist <71245049+Mykolahist@users.noreply.github.com>
2022-02-09 12:24:32 +0200

    Updates

--- index.0641b553.js
+++ index.0641b553.js
@@ -519,6 +519,64 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"bNKaB":[function(require,module,exports) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+var _accordion = require("./js/accordion");
+var _accordionDefault = parcelHelpers.interopDefault(_accordion);
+
+},{"./js/accordion":"7H7LO","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"7H7LO":[function(require,module,exports) {
+const refs = {
+    questionItem: document.querySelectorAll('.questions-top'),
+    accordionBtn: document.querySelectorAll('.questions-btn'),
+    questionTop: document.querySelectorAll('.questions-top'),
+    openText: document.querySelectorAll('.detailed-questions'),
+    closeBtn: document.querySelectorAll('.close-btn')
+};
+refs.accordionBtn.forEach((e)=>e.addEventListener('click', onOpenAccordion)
+);
+refs.closeBtn.forEach((e)=>e.addEventListener('click', onCloseAccordion)
+);
+function onOpenAccordion() {
+    // this.parentNode.parentNode.parentNode.parentNode.querySelector('.questions-item').classList.toggle('new-item');
+    this.parentNode.parentNode.parentNode.parentNode.querySelector('.questions-top').classList.add('new-container');
+    this.parentNode.parentNode.parentNode.parentNode.querySelector('.detailed-questions').classList.add('open');
+// this.parentNode.parentNode.parentNode.parentNode.querySelector('.questions-text').classList.toggle('new-text');
+}
+function onCloseAccordion() {
+    const open = this.parentNode.parentNode.parentNode.parentNode.querySelector('.detailed-questions');
+    const top = this.parentNode.parentNode.parentNode.parentNode.querySelector('.questions-top');
+    if (open.classList.contains('open')) open.classList.remove('open');
+    if (top.classList.contains('new-container')) top.classList.remove('new-container');
+}
+
+},{}],"gkKU3":[function(require,module,exports) {
+exports.interopDefault = function(a) {
+    return a && a.__esModule ? a : {
+        default: a
+    };
+};
+exports.defineInteropFlag = function(a) {
+    Object.defineProperty(a, '__esModule', {
+        value: true
+    });
+};
+exports.exportAll = function(source, dest) {
+    Object.keys(source).forEach(function(key) {
+        if (key === 'default' || key === '__esModule' || dest.hasOwnProperty(key)) return;
+        Object.defineProperty(dest, key, {
+            enumerable: true,
+            get: function() {
+                return source[key];
+            }
+        });
+    });
+    return dest;
+};
+exports.export = function(dest, destName, get) {
+    Object.defineProperty(dest, destName, {
+        enumerable: true,
+        get: get
+    });
+};
 
 },{}]},["6IXwR","bNKaB"], "bNKaB", "parcelRequire89f3")
 
